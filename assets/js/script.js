503f6b9d87f73b079e71dc51b9312bac99e32cca
Rohan Kedaria <156511117+SKYWALKER199x@users.noreply.github.com>
2024-03-14 01:02:58 +0530

    1:10

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ const addEventOnElements = function (elements, eventType, callback){
  * Navbar toggler for mobile
  */
 const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelector("[data-nav-toggler]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
 
 const toggleNav = function(){
@@ -28,3 +28,13 @@ const toggleNav = function(){
 }
 
 addEventOnElements(navTogglers, "click", toggleNav);
+
+/**
+ * Header
+ */
+
+const header = document.querySelector("[data-header]");
+
+Window.addEventlistener('scroll',function() {
+   header.classList[window.scrollY > 100 ? "add" : "remove"]("active"); 
+});
