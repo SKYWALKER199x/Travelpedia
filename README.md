912178cc696405dc110e998fb26ca0b507478c80
Rohan Kedaria <156511117+SKYWALKER199x@users.noreply.github.com>
2024-03-18 21:51:41 +0530

    All Done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Travelia-Traval-Agency
+Landing Page for the Travelia Travel Agency Service, Clean UI and Interactive Design
+## Live Link: [https://traveliaagency.netlify.app/](https://traveliaagency.netlify.app/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,32 @@
 const preloader = document.querySelector("[data-preloader]");
 
 window.addEventListener("load", () => {
-    preloader.classList.add("remove");
+  preloader.classList.add("remove");
 });
 
 /**
- * add event on multiple elements
+ * Add event on multiple element
  */
 
-const addEventOnElements = function (elements, eventType, callback){
-    for (let i = 0, len=elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType, callback);      
-    }
-}
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
 
 /**
- * Navbar toggler for mobile
+ * Navbar toggle for mobile
  */
+
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
 
-const toggleNav = function(){
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-    document.body.classList.toggle("nav-active"); 
-}
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
 
 addEventOnElements(navTogglers, "click", toggleNav);
 
@@ -34,7 +35,6 @@ addEventOnElements(navTogglers, "click", toggleNav);
  */
 
 const header = document.querySelector("[data-header]");
-
-Window.addEventlistener('scroll',function() {
-   header.classList[window.scrollY > 100 ? "add" : "remove"]("active"); 
+window.addEventListener("scroll", function () {
+  header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
 });
